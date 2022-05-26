80325db99cbd1461e0861b24967d6859c602802f
Rathes Sachchithananthan <rathes@aheenam.com>
2022-05-26 14:00:23 +0100

    ✅ Ensure it builds iso 15919 transliteration

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,13 @@
-import script from './index';
+import script, { getIso15919 } from './index';
 
 describe('script tests', () => {
   it('exports scripts from json', () => {
     expect(script).toMatchSnapshot();
     expect(Array.isArray(script)).toBeTruthy();
     expect(script.length).toBe(19);
+  });
+
+  it('builds the iso 15919 for word', () => {
+    expect(getIso15919('அம்மா')).toEqual('ammā');
   });
 });
